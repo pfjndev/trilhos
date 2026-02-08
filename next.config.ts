4dbdea760656b5336acb28d7b1179055d4868dbf
pfjndev <88943477+pfjndev@users.noreply.github.com>
2026-02-08 18:57:44 +0000

    fix: docker deployment

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  output: 'standalone',
+  allowedDevOrigins: ['localhost', 'trilhos.indiosdainsua.casa'],
   async headers() {    
     return [
       {
         source: "/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "local-origin.dev, *.local-origin.dev" },
+          { key: "Access-Control-Allow-Origin", value: "localhost" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
         ],
